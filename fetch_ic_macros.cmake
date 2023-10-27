c8c5d198694fa9157523281e793512ecba8809c4
J.P. Hutchins <jphutchins@gmail.com>
2023-10-26 20:53:42 -0700

    fix: support CMake <= 3.24.0

--- fetch_ic_macros.cmake
+++ fetch_ic_macros.cmake
@@ -5,10 +5,14 @@
 
 include_guard(GLOBAL)
 
+if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.24.0")
+    set(download_extract_timestamp "DOWNLOAD_EXTRACT_TIMESTAMP true")
+endif()
+
 include(FetchContent)
 
 FetchContent_Declare(ic_macros
     URL https://github.com/intercreate/ic-macros/releases/latest/download/ic_macros.zip
-    DOWNLOAD_EXTRACT_TIMESTAMP true
+    ${download_extract_timestamp}
 )
 FetchContent_MakeAvailable(ic_macros)
